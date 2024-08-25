642171b0e47f71f473b3dfda7ebd532817d4ba7c
scorpiraq <96728173+scorpiraq@users.noreply.github.com>
2024-08-25 16:20:47 +0300

    clean unnecessary code server.js

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-const express = require('express')
- // const app = express()
-// const port = 1010
-//
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
-//
-// app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}`)
-// })
-
 const app = require('./app');
 
 // Define the port to listen on
